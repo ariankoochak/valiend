5ffa831618c90c7e88015fa09f0f67b7c1bbef01
Arian <arian.koochak@protonmail.com>
2024-08-31 10:06:42 +0330

    fix - catch errors

--- source/index.js
+++ source/index.js
@@ -4,13 +4,17 @@
  * @return {Boolean} If the input string is an email, it returns true and if the input string is not an email, it returns false.
  */
 function isEmail(str) {
-    if (typeof str !== "string" || str.length === 0) {
-        return false;
+    try {
+        if (typeof str !== "string" || str.length === 0) {
+            return false;
+        }
+        const emailRegex =
+            /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+        const match = str.match(emailRegex);
+        return match !== null ? match.input === match[0] : false;
+    } catch (error) {
+        return false
     }
-    const emailRegex =
-        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    const match = str.match(emailRegex);
-    return match !== null ? match.input === match[0] : false;
 }
 
 /**
@@ -21,16 +25,20 @@ function isEmail(str) {
  * @return {Boolean} If the input string is an phone number, it returns true and if the input string is not an phone number, it returns false.
  */
 function isPhoneNumber(str, options = { ignoreCountryCode: false }) {
-    if (typeof str !== "string" || str.length === 0) {
-        return false;
+    try {
+        if (typeof str !== "string" || str.length === 0) {
+            return false;
+        }
+        const phoneNumberDefaultRegex =
+            /(\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{6,14}$)/;
+        const phoneNumberIgnoreCountryCodeRegex = /[0-9]{4,12}/;
+        const match = options.ignoreCountryCode
+            ? str.match(phoneNumberIgnoreCountryCodeRegex)
+            : str.match(phoneNumberDefaultRegex);
+        return match !== null ? match.input === match[0] : false;
+    } catch (error) {
+        return false
     }
-    const phoneNumberDefaultRegex =
-        /(\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{6,14}$)/;
-    const phoneNumberIgnoreCountryCodeRegex = /[0-9]{4,12}/;
-    const match = options.ignoreCountryCode
-        ? str.match(phoneNumberIgnoreCountryCodeRegex)
-        : str.match(phoneNumberDefaultRegex);
-    return match !== null ? match.input === match[0] : false;
 }
 
 /**
@@ -39,6 +47,7 @@ function isPhoneNumber(str, options = { ignoreCountryCode: false }) {
  * @return {object} An object containing string information
  */
 function passwordContains(str) {
+    try {
         if (typeof str !== "string" || str.length === 0) {
             return {};
         }
@@ -62,6 +71,9 @@ function passwordContains(str) {
             numberCount,
             characterCount,
         };
+    } catch (error) {
+        return {}
+    }
 }
 
 /**
@@ -70,16 +82,20 @@ function passwordContains(str) {
  * @return {object} An object containing Email information
  */
 function separateEmail(str){
-    if (isEmail(str) === false) {
-        return {};
+    try {
+        if (isEmail(str) === false) {
+            return {};
+        }
+        return {
+            emailUsername: str.split("@")[0],
+            emailSymbol: "@",
+            emailMailServer: str.split("@")[1].split(".")[0],
+            emailMailServerUrl: str.split("@")[1],
+            emailDomain: str.split("@")[1].replace(str.split("@")[1].split(".")[0],''),
+        };
+    } catch (error) {
+        return {}
     }
-    return {
-        emailUsername: str.split("@")[0],
-        emailSymbol: "@",
-        emailMailServer: str.split("@")[1].split(".")[0],
-        emailMailServerUrl: str.split("@")[1],
-        emailDomain: str.split("@")[1].replace(str.split("@")[1].split(".")[0],''),
-    };
 }
 
 
