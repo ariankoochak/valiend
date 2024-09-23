3f7846fd9a7aa0a273b09b0de2e892c6876f3499
Arian <arian.koochak@protonmail.com>
2024-09-23 13:35:35 +0330

    fix - fix test with new passwordQuality

--- source/test/passwordQuality.test.js
+++ source/test/passwordQuality.test.js
@@ -5,9 +5,9 @@ test("passwordQuality => 'aadfdf' :: 0", () => {
 });
 
 test("passwordQuality => '78788989sdsdf' :: 50", () => {
-    expect(valiend.passwordQuality("78788989sdsdf")).toBe(50);
+    expect(valiend.passwordQuality("78788989sdsdf")).toBe(45);
 });
 
 test("passwordQuality => 'A5h#3232' :: 100", () => {
-    expect(valiend.passwordQuality("A5h#3232")).toBe(100);
+    expect(valiend.passwordQuality("A5h#3232")).toBe(85);
 });
