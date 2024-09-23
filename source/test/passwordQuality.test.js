2975c65059e3bc943f150b70289801778f1c73a9
Arian <arian.koochak@protonmail.com>
2024-09-23 13:40:33 +0330

    add new test in passwordQuality tests

--- source/test/passwordQuality.test.js
+++ source/test/passwordQuality.test.js
@@ -11,3 +11,7 @@ test("passwordQuality => '78788989sdsdf' :: 50", () => {
 test("passwordQuality => 'A5h#3232' :: 100", () => {
     expect(valiend.passwordQuality("A5h#3232")).toBe(85);
 });
+
+test("passwordQuality => 'A5h#3232' :: 100", () => {
+    expect(valiend.passwordQuality("Al2#B3k@P-e5")).toBe(100);
+});
