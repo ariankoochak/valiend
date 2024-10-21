e27342941665142d9c6ef9d719a9058e8e4aa13f
Arian <arian.koochak@protonmail.com>
2024-10-21 19:20:30 +0330

    add postal code function but i should add postal code regex to utils

---
+++ source/lib/isPostalCode.js
@@ -0,0 +1,22 @@
+/**
+ * With this method, you can check whether the zip code entered is valid or not
+ * @param {string} postalCode
+ * @param {Array} options.regions An array of the list of valid regions where the postal code can be in one of the entered regions
+ * @returns {boolean}
+ */
+function isPostalCode(postalCode,options = {regions : []}) {
+    try {
+        const {regions} = options;
+        if (typeof postalCode !== "string" || postalCode.length === 0 || regions === undefined ||regions.length === 0) {
+            return false;
+        }
+        for(const region of regions){
+            console.log(region);
+        }
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+module.exports = isPostalCode;
