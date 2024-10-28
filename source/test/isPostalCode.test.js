f7be83fd9abea934f330f7b8ddbcf652742395e1
Arian <arian.koochak@protonmail.com>
2024-10-28 10:37:10 +0330

    add suggest for regions standards

--- source/test/isPostalCode.test.js
+++ source/test/isPostalCode.test.js
@@ -1,5 +1,5 @@
 const valiend = require("../index");
 
 test("isPostalCode => '12345-12345' :: false", () => {
-    expect(valiend.isPostalCode("12345-12345", {regions : ["fa-IR"]})).toBe(true);
+    expect(valiend.isPostalCode("12345-12345",["fa-IR"])).toBe(true);
 });
